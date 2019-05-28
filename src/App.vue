0a1dbbf532d786bf8ddd9881f75901a24921336f
Hannah Hudson <hannah.hch@gmail.com>
2019-05-28 17:58:49 -0400

    started proj

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <nav class="nav-links">
+        <div class="nav-links-wrapper">
+          <router-link to="/contact" class="nav-links__link">Contact</router-link>
+          <router-link to="/">
+            <h2 class="nav-title">Hannah Hudson</h2>
+          </router-link>
+          <router-link to="/about" class="nav-links__link">About</router-link>
+        </div>
+      </nav>
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
+
+body {
+  margin: 0;
+  color: #2d3436;
+  background-color: #f2f2f2;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+a {
+  text-decoration: none;
+}
+
+.nav-links {
+  background-color: #fff;
+  box-shadow: 0 0 20px 0 rgba(0,0,0,.1);
+}
+
+.nav-links-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+.nav-title {
+  margin: 10px;
+  font-size: 45px;
+  letter-spacing: 5px;
+  font-family: 'Mr Dafoe', cursive;
+  text-decoration: none;
+  color: #2d3436;
+}
+
+.nav-links__link {
+  text-decoration: none;
+  color: #2d3436;
+  font-size: 18px;
 }
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,73 @@
+<template lang="html">
+  <section class="portfolio-items">
+      <div class="portfolio-item portfolio-item--ng"></div>
+      <div class="portfolio-item portfolio-item--fb"></div>
+      <div class="portfolio-item portfolio-item--hc"></div>
+      <div class="portfolio-item portfolio-item--rourk"></div>
+      <div class="portfolio-item portfolio-item--econo"></div>
+      <div class="portfolio-item portfolio-item--fbomb"></div>
+      <div class="portfolio-item portfolio-item--tour">
+        <span>tourify</span>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+  .portfolio-items {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-gap: 50px;
+    max-width: 1000px;
+    margin: 50px auto;
+  }
+  .portfolio-item {
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: top;
+    height: 1px;
+    padding-top: calc(100 / 100 * 100%);
+  }
+
+  .portfolio-item--ng {
+    background-image: url('../assets/ng.png');
+  }
+
+  .portfolio-item--fb {
+    background-image: url('../assets/fb.jpg');
+  }
+
+  .portfolio-item--hc {
+    background-image: url('../assets/hc.jpg');
+  }
+
+  .portfolio-item--rourk {
+    background-image: url('../assets/rourk.png');
+  }
+
+  .portfolio-item--econo {
+    background-image: url('../assets/econo.jpg');
+  }
+
+  .portfolio-item--fbomb {
+    background-image: url('../assets/fbomb.jpg');
+  }
+
+  .portfolio-item--tour {
+    position: relative;
+    background-color: #E34008;
+    color: white;
+  }
+
+  .portfolio-item--tour  > span {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 60px;
+  }
+</style>
