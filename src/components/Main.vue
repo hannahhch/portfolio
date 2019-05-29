dd648efbce70537a7357e27b52955dec79361ff1
Hannah Hudson <hannah.hch@gmail.com>
2019-05-29 18:26:37 -0400

    added json

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,19 +1,30 @@
 <template lang="html">
   <section class="portfolio-items">
-      <div class="portfolio-item portfolio-item--ng"></div>
-      <div class="portfolio-item portfolio-item--fb"></div>
-      <div class="portfolio-item portfolio-item--hc"></div>
-      <div class="portfolio-item portfolio-item--rourk"></div>
-      <div class="portfolio-item portfolio-item--econo"></div>
-      <div class="portfolio-item portfolio-item--fbomb"></div>
-      <div class="portfolio-item portfolio-item--tour">
-        <span>tourify</span>
-      </div>
+    <div
+      class="portfolio-item"
+      v-for="project in projects"
+      :style="{
+        'background-image': 'url(' + require(`../assets/${project.image}`) + ')'
+        }"
+    >
+    </div>
   </section>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+  data: function (){
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+     axios.get('../data/projects.json').then(response => {
+       this.projects = response.data.projects;
+     })
+   }
 }
 </script>
 
@@ -31,43 +42,5 @@ export default {
     background-position: top;
     height: 1px;
     padding-top: calc(100 / 100 * 100%);
-  }
-
-  .portfolio-item--ng {
-    background-image: url('../assets/ng.png');
-  }
-
-  .portfolio-item--fb {
-    background-image: url('../assets/fb.jpg');
-  }
-
-  .portfolio-item--hc {
-    background-image: url('../assets/hc.jpg');
-  }
-
-  .portfolio-item--rourk {
-    background-image: url('../assets/rourk.png');
-  }
-
-  .portfolio-item--econo {
-    background-image: url('../assets/econo.jpg');
-  }
-
-  .portfolio-item--fbomb {
-    background-image: url('../assets/fbomb.jpg');
-  }
-
-  .portfolio-item--tour {
-    position: relative;
-    background-color: #E34008;
-    color: white;
-  }
-
-  .portfolio-item--tour  > span {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    font-size: 60px;
   }
 </style>
