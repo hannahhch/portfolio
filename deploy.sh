0794780b8e2425ed3094da6fdce0ebdbc120033b
Hannah Hudson <hannah.hch@gmail.com>
2019-06-17 22:06:27 -0400

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+echo 'www.hannahhch.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<hannahhch>.github.io/<portfolio>
+git push -f https://github.com/hannahhch/portfolio.git master:gh-pages
+cd -
