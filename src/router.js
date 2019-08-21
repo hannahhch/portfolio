2949870a38286c8bedbdfca95bc68149d184e3bc
Hannah Hudson <hahudson@cisco.com>
2019-08-21 15:45:04 -0600

    rm history mode

--- src/router.js
+++ src/router.js
@@ -1,25 +1,32 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Main from './views/Main.vue'
+import Resume from './views/Resume.vue'
+import Project from './views/Project.vue'
+
 Vue.use(Router)
 
 export default new Router({
-  // mode: 'history',
+  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    // { path: '*',
+    //   component: NotFoundComponent
+    // },
     {
       path: '/',
       name: 'main',
-      component: () => import(/* webpackChunkName: "about" */ './views/Main.vue')
+      component: Main
     },
     {
       path: '/resume',
       name: 'resume',
-      component: () => import(/* webpackChunkName: "resume" */ './views/Resume.vue')
+      component: Resume
     },
     {
       path: '/projects/:id',
-      component: () => import(/* webpackChunkName: "project" */ './views/Project.vue')
+      component: Project
     }
   ]
 })
