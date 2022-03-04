9f14ae208a52146dacd3a15a9c590bfa49be9ea7
Hannah Hudson <hannah.hch@gmail.com>
2022-03-04 14:00:19 -0700

    Update about

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,13 +2,15 @@
   <section class="about-section">
     <div class="about-section--wrapper">
       <h2>Hello, I'm Hannah.</h2>
-      <h3>I am a frontend engineer and educator currently living in Denver, Colorado.</h3>
-      <p>Before completing <a href="https://www.coursereport.com/schools/the-iron-yard" target="_blank" rel="noreferrer">The Iron Yard's</a> web development program in 2017, I was a high school special education teacher. I enjoyed many aspects of teaching special eduation (especially working with my students and teaching them life skills), but found that I wanted a change. Software engineering gave me a career that encouraged life-long learning, and I loved the creativity of working on the frontend.</p>
-      <p>I started my developer career at an agency in Chapel Hill, NC as a frontend developer, where I fell in love with CSS. I had the opportunity to build complex layouts, learn about browser inconsistancies, google analytics and web accessibility. <p>
-      <p>After working at Rivers Agency, I went to a company called Smashing Boxes. Here I learned Angular, React Native and got more comfortable working with React. I enjoyed having so much variety, but still used my CSS skills often! I was able to dive deeper, and work on a project with CSS animations. </p>
-      <p>Recently, I started working as an assistant instructor at <a href="https://turing.io/" target="_blank">The Turing School of Software and Design</a>. I am incredibly excited for this new opportunity to combine my teaching and developer skillsets!</p>
-        
-        <p>In my free time, I like to <a target="_blank" href="https://www.instagram.com/tartsbyhannah/">bake pastries</a> - the more complicated, the better! I also enjoy hiking, rock climbing, traveling and hanging out with my cat and husband. </p>
+      <h3>I am a frontend engineer and educator currently living in Denver, Colorado. I currently work as an Instructor at the <a href="https://turing.io/">Turing School of Software and Design</a>.</h3>
+      <p>My hobbies change up pretty frequently, but I'm currently excited about:</p>
+      <ul class="about-section--list">
+        <li>Scuba diving</li>
+        <li>Traveling</li>
+        <li>Swimming</li>
+        <li>Caves</li>
+        <li>Weird earrings</li>
+      </ul>
     </div>
   </section>
 </template>
@@ -31,6 +33,17 @@ export default {
 
   p {
     margin: 0 0 10px;
+  }
+
+  .about-section--list li {
+    font-size: 18px;
+    list-style: none;
+    margin: 0;
+  }
+
+  .about-section--list li:before {
+    content: '●';
+    margin-right: 5px;
   }
 
   .about-section--wrapper {
