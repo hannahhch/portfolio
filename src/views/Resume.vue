f6bf732a1a50741eeeac9799a0e69d7fc956d25c
Hannah Hudson <hannah.hch@gmail.com>
2022-03-21 13:59:32 -0600

    Add resume data

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -3,153 +3,80 @@
     <div class="resume-header__wrapper">
       <div class="resume-header">
         <div>
-          <h1 class="resume-header--title">Hannah Hudson</h1>
-          <h2 class="resume-header--subtitle">Frontend Software Engineer</h2>
+          <h1 class="resume-header--title">{{ resume.title }}</h1>
+          <h2 class="resume-header--subtitle">{{ resume.subtitle }}</h2>
         </div>
         <img class="resume-image" src="../assets/images/profile.jpg" alt="profile of Hannah Hudson">
       </div>
     </div>
 
     <div class="wrapper">
-        <h2 class="section-title">
-          Professional Experience
-        </h2>
-          <h3 class="info-title">
-          <div> <em><span class="font--bold">Frontend Instructor</span> | <a href="https://turing.io" target="_blank" rel="noreferrer">The Turing School of Software and Design</a> | Denver, CO -> Remote</em></div>
-          <div class="blue--accent">October 2019 - March 2022</div>
-        </h3>
-        <ul class="experience-list">
-          <li>Created new resources, projects and exercises for students. One example of this is a bank of <a href="https://10daysfrontend.netlify.app/" target="_blank" rel="noreferrer">advent calendar-style exercises</a> for extra repetition practice.</li>
-          <li>Mentored new instructors; providing instructional support and guidance. Assisted many of these instructors on their own growth path at Turing. I also created tangible resources to help with onboarding.</li>
-          <li>Continuously iterated on improving the frontend curriculum by documenting pain points (for both students and instructors) from one section of the program to the next.</li>
-          <li>Taught over 400 students frontend engineering skills.</li>
-        </ul>
-        <ul class="skills-list">
-          <li>React</li>
-          <li>Mocha</li>
-          <li>Chai</li>
-          <li>Web Accessibility</li>
-          <li>JavaScript</li>
-          <li>Git</li>
-          <li>GitHub</li>
-          <li>CSS/Sass</li>
-          <li>HTML</li>
-        </ul>
-        <hr>
+      <h2 class="section-title">
+        Professional Experience
+      </h2>
+      <section class="section" v-for="exp in resume.experience">
         <h3 class="info-title">
-          <div> <em><span class="font--bold">Software Engineer</span> | <a href="https://smashingboxes.com/" target="_blank" rel="noreferrer">Smashing Boxes</a> | Durham, NC -> Remote</em></div>
-          <div class="blue--accent">February 2019 - October 2019</div>
+          <div> <em><span class="font--bold">{{ exp.role }}</span> | <a :href="exp.company.link" target="_blank" rel="noreferrer">{{ exp.company.name }}</a> | {{ exp.location }}</em></div>
         </h3>
-        <ul class="experience-list">
-          <li>Became the in-house expert for building CSS micro animations and debugging browser inconsistencies.</li>
-          <li>Quickly learned to work with multiple technologies to build frontend applications to meet the needs of clients of various sizes, from startups to enterprises like Cisco.</li>
+        <h4 class="blue--accent">{{ exp.dates }}</h4>
+        <ul v-if="exp.duties" v-for="duty in exp.duties" class="experience-list">
+          <li>{{ duty }}</li>
         </ul>
-        <ul class="skills-list">
-          <li>Angular</li>
-          <li>TypeScript</li>
-          <li>React</li>
-          <li>React Native</li>
-          <li>Git</li>
-          <li>Git</li>
-          <li>CSS Animation</li>
-          <li>CSS/Sass</li>
-          <li>HTML</li>
+        <ul v-if="exp.skills" class="skills-list">
+          <li v-for="skill in exp.skills">{{ skill }}</li>
         </ul>
-        <hr>
-        <h3 class="info-title">
-          <div><em> <span class="font--bold">Frontend Developer</span> | <a href="https://riversagency.com/" target="_blank" rel="noreferrer">Rivers Agency</a> | Chapel Hill, NC</em></div>
-          <div class="blue--accent">November 2017 - February 2019</div>
-        </h3>
-        <ul class="experience-list">
-          <li>Attended a training in web accessibility principles, and brought back what I learned to my coworkers through lunch and learn sessions. This decreased last minute accessibility fixes, and added accessibility compliance to our service offerings.</li>
-          <li>
-            Built responsive websites to meet the needs of multiple clients. I provided training on content entry and site maintenance to clients as a part of the handoff at the end of engagements.
-          </li>
-        </ul>
-        <ul class="skills-list">
-          <li>Vue</li>
-          <li>PHP</li>
-          <li>Shopify</li>
-          <li>Wordpress</li>
-          <li>Web Accessibility</li>
-          <li>Git</li>
-          <li>Bitbucket</li>
-          <li>CSS/Sass/LESS</li>
-          <li>HTML</li>
-        </ul>
-        <hr>
-        <h3 class="info-title">
-          <div><em><span class="font--bold">Special Education Teacher</span> | Chapel Hill High School, NC</em></div>
-          <div class="blue--accent">August 2016 - June 2017</div>
-        </h3>
-        <hr>
-        <h3 class="info-title">
-          <div><em><span class="font--bold">Special Education Teacher</span> | Rolesville High School, NC</em></div>
-          <div class="blue--accent">August 2014 - June 2016</div>
-        </h3>
-        <hr>
+      </section>
 
       <section>
         <h2 class="section-title">
           Education
         </h2>
-        <h3 class="info-title">
-            <div><em> <a href="https://www.coursereport.com/schools/the-iron-yard" target="_blank" rel="noreferrer">The Iron Yard</a> | Durham, NC | Web Development Path</em></div>
-            <div class="blue--accent">July 2017- October 2017</div>
-        </h3>
-        <hr>
-        <h3 class="info-title">
-          <div><em>University of NC Wilmington | B.A in Special Education</em></div>
-          <div class="blue--accent">Graduated July 2014</div>
-        </h3>
-
+        <section v-for="education in resume.education" class="section">
+           <h3 class="info-title">
+            <div><em> <a :href="education.link" target="_blank" rel="noreferrer">{{ education.school }}</a> | {{ education.location }} | {{ education.focus}}</em></div>
+            <div class="blue--accent">{{ education.dates }}</div>
+         </h3>
+        </section>
       </section>
-      <hr>
+
       <section>
         <h2 class="section-title">
           Community Activities
         </h2>
-        <h3 class="info-title">
-          <div><em>
-            <a target="_blank" rel="noreferrer" href="https://turing.io">Volunteer for Code with Joan</a> | Volunteer teacher for coding workshop for women+</em></div>
-          <div class="blue--accent">February 2022</div>
-        </h3>
-        <hr>
-        <h3 class="info-title">
-          <div><em>
-            <a target="_blank" rel="noreferrer" href="https://www.momentumlearn.com/">Momentum Ambassador</a> | Mentor and volunteer teacher for  local code school</em></div>
-          <div class="blue--accent">March 2018 - August 2019</div>
-        </h3>
-        <hr>
-        <h3 class="info-title">
-          <div><em>
-            <a href="https://webaim.org/training/" target="_blank" rel="noreferrer">WebAIM Training</a>
-              | Attended two day course on web accessibility principles</em></div>
-          <div class="blue--accent">September 2018</div>
-        </h3>
-        <hr>
-        <h3 class="info-title">
-          <div><em>Co-Teacher for <a href="https://tip.duke.edu/" target="_blank" rel="noreferrer">Duke TIP Program</a> | Full day programming course for 5th - 6th graders</em></div>
-          <div class="blue--accent">July 2018</div>
-        </h3>
-        <hr>
-        <h3 class="info-title">
-          <div><em>Teacher for <a href="https://www.girldevelopit.com/" target="_blank" rel="noreferrer">Girl Develop It</a> | Taught Intermediate HTML/CSS (8 hrs)</em></div>
-          <div class="blue--accent">March 2018</div>
-        </h3>
-
+        <section v-for="community in resume.community" class="section">
+          <h3 class="info-title">
+            <div><em>
+              <a target="_blank" rel="noreferrer" :href="community.link">{{ community.title }}</a> | {{ community.role }}</em></div>
+            <div class="blue--accent">{{ community.dates }}</div>
+          </h3>
+        </section> 
       </section>
     </div>
   </main>
 </template>
 
-<style scoped lang="css">
-  hr {
-    border-color: rgb(87, 101, 116, .1);
-    margin: 25px 0 ;
-  }
+<script>
+import axios from 'axios';
 
-  section {
+export default {
+    data: function (){
+    return {
+     resume: {}
+    }
+  },
+   mounted() {
+     axios.get('../data/resume.json').then(response => {
+       this.resume = response.data.resume;
+     });
+   }
+}
+
+</script>
+
+<style scoped lang="css">
+  .section {
+    border-bottom: solid 2px rgb(87, 101, 116, .1);
+    padding-bottom: 15px;
     margin-bottom: 25px;
   }
 
